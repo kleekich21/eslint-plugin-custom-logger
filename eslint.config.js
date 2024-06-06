745fd6204d46540e6c6d833d5bc5f1d598de07ee
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-06 13:41:32 +0900

    feat: add eslint flat config file to apply ESLint rule locally.

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const customLoggerPlugin = require("./custom-logger-plugin");
+
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      ecmaVersion: "latest",
+    },
+    plugins: { "custom-logger": customLoggerPlugin },
+    rules: {
+      "custom-logger/suggest-custom-logger": "warning",
+    },
+  },
+];
