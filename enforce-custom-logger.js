457e3c8ca558845784c7cb49d4ac7b59087cb0e2
kleekich21 <kangsik.kevin.lee+1@gmail.com>
2024-06-06 13:25:49 +0900

    feat: add ESlint rule logic with fix for all "console.log"

--- enforce-custom-logger.js
+++ enforce-custom-logger.js
@@ -2,12 +2,37 @@ module.exports = {
   meta: {
     type: "suggestion",
     docs: {
-      description: "console.log 대신 커스텀 logger를 사용하는 것을 권장한다.",
+      description: "console.log 대신 Customlogger를 사용하는 것을 권장한다.",
     },
     fixable: "code",
     schema: [],
   },
   create(context) {
-    return {};
+    return {
+      CallExpression(node) {
+        if (
+          node.callee.object?.name === "console" &&
+          node.callee.property.name === "log"
+        ) {
+          context.report({
+            node,
+            message:
+              "customLogger가 아닌 일반 console 로그가 사용되었습니다. Unexpected Logger: {{ usedLogger }}",
+            data: {
+              usedLogger: node.callee.object.name,
+            },
+            fix(fixer) {
+              const sourceCode = context.getSourceCode();
+              const currLogCall = sourceCode.getText(node);
+              const fixedLogCall = currLogCall.replace(
+                "console.log",
+                "CustomLogger.log"
+              );
+              return fixer.replaceText(node, fixedLogCall);
+            },
+          });
+        }
+      },
+    };
   },
 };
